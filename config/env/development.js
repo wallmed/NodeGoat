185af2874cb798eef322285d8dce207d4f499bab
wallmed <wall.rod.med@gmail.com>
2021-07-15 09:51:27 -0300

    second chance

--- config/env/development.js
+++ config/env/development.js
@@ -3,6 +3,7 @@ module.exports = {
    zapHostName: "192.168.56.20",
    zapPort: "8080",
    //Strange port
+   //Another strange line
    // Required from Zap 2.4.1. This key is set in Zap Options -> API _Api Key.
    zapApiKey: "v9dn0balpqas1pcc281tn5ood1",
    // Required if debugging security regression tests.
