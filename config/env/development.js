47c04f5f949d92ede7b38fc03c65d03f76b8d990
wallmed <wall.rod.med@gmail.com>
2021-07-15 09:48:10 -0300

    alguma mensagem

--- config/env/development.js
+++ config/env/development.js
@@ -2,6 +2,7 @@ module.exports = {
    // If you want to debug regression tests, you will need the following which is also in the test config:
    zapHostName: "192.168.56.20",
    zapPort: "8080",
+   //Strange port
    // Required from Zap 2.4.1. This key is set in Zap Options -> API _Api Key.
    zapApiKey: "v9dn0balpqas1pcc281tn5ood1",
    // Required if debugging security regression tests.
